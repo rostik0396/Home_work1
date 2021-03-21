8b09aa1f02f8facf0955bc1c174ae1be32c04494
rostik0396 <80713641+rostik0396@users.noreply.github.com>
2021-03-21 18:31:55 +0200

    HomeWork2

---
+++ index.js
@@ -0,0 +1,44 @@
+var message = 'Hello World'; // инициализвция пременом
+var text;  //обьявление новой переменной
+text = message;
+
+console.log (message);
+console.log (text);
+
+var MAIN_COLOR = 'FF7F00';
+var color = MAIN_COLOR;
+console.log (MAIN_COLOR);
+
+// 1 string - строка
+// 2 number - числов
+// 3 boolean - true | fasle
+// 4 undefined - если неприсвоенно значение
+// 5 null - ничего
+// symbol - символы
+// object - не примитивный тип данных
+// ... - помогает создавать уникальные индификаторы
+// bigInt - позволяет работать с огромными числами
+
+var message = 'I\'m JS developer';
+var message = "Hello\nWorld!";
+var message = '';
+
+
+var number = 0x2A; // число 42 в шестнадцатиричной системе
+
+// infinty самое большое или самое маленькое число
+
+var result = 0/0; // NaN
+var result = "Hello World" * 42;  //NaN
+
+
+var isMenuOpen = true;
+var isLogedIn = false;   // булевые значения
+
+// typeof - проверка типов
+
+var a = 40;
+var b = 2;
+
+var result = a + b;
+console.log(result);
